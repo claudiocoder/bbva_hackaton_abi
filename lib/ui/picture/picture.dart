153bcbc496933efa665cbe7107dd023df708fbbf
cmor <clauartur98@gmail.com>
2020-10-16 23:31:55 -0500

    feat: add camera functionality

--- lib/ui/picture/picture.dart
+++ lib/ui/picture/picture.dart
@@ -1,13 +1,39 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class PictureScreen extends StatelessWidget {
+class PictureScreen extends StatefulWidget {
+  @override
+  _PictureScreenState createState() => _PictureScreenState();
+}
+
+class _PictureScreenState extends State<PictureScreen> {
+
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera, preferredCameraDevice: CameraDevice.front);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SafeArea(
-        child: Text(
-          'Aqui tomaremos la foto',
-        ),
+    return Scaffold(
+      body: Center(
+        child: _image == null
+            ? Text('Ninguna Imagen seleccionada')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Tomar Foto',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
